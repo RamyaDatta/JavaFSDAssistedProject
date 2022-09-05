340f0beea3cfe14038d440724e765e439115f2ea
RamyaDatta <ramyadatta6@gmail.com>
2022-09-06 00:05:15 +0530

    Online QuizPortal

---
+++ OnlineQuizPortal/src/main/java/com/repository/QuizRepo.java
@@ -0,0 +1,20 @@
+package com.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.entity.Quiz;
+@Repository
+
+
+public interface QuizRepo extends JpaRepository<Quiz, Integer> {
+	
+	@Query("select q.title,count(distinct q.quizno) from Quiz as q group by q.quizno")
+	public List<Object> listOfQuiz();
+	
+
+
+}
---
+++ OnlineQuizPortal/src/main/java/com/repository/UserRepo.java
@@ -0,0 +1,16 @@
+package com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.entity.User;
+
+@Repository
+
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+	public User findByEmailid(String emailid);
+
+	
+
+}
