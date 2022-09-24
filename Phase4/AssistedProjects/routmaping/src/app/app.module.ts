f4a81633c98cfa44ae3cd7c2671a99ce2312c401
RamyaDatta <ramyadatta6@gmail.com>
2022-09-24 18:09:31 +0530

    Phase 4 assisted and practice projects

---
+++ Phase4/AssistedProjects/routmaping/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PipesComponent } from './pipes/pipes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    PipesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
